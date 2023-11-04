ca77dccbd702337b0fb598852b592eb8786cbc83
Rashed <rashedulhoqueceo@gmail.com>
2023-11-05 02:20:27 +0600

    create a banner

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,34 @@
+import img from "../../assets/Images/banner.png"
+
+const Banner = () => {
+    return (
+        <div className="container mx-auto px-2 md:px-6 py-8 md:py-16 lg:py-24 flex flex-col-reverse md:flex-row items-center gap-10">
+            <div className="flex-1">
+                <h2 className="text-xl lg:text-4xl text-[#1F4B3F] font-bold mb-8">More than 10K expert <br /> freelancers are waiting for you</h2>
+                <div className="flex items-center justify-between gap-6">
+                    <div>
+                        <h4 className="text-lg lg:text-3xl font-bold text-[#1F4B3F] ">835M</h4>
+                        <p className="text-[#222222] text-xs font-light lg:text-base lg:font-normal">Total Freelancer</p>
+                    </div>
+                    <div>
+                        <h4 className="text-lg lg:text-3xl font-bold text-[#1F4B3F]">750M</h4>
+                        <p className="text-[#222222] text-xs font-light lg:text-base lg:font-normal">Positive Review</p>
+                    </div>
+                    <div>
+                        <h4 className="text-lg lg:text-3xl font-bold text-[#1F4B3F]">90M</h4>
+                        <p className="text-[#222222] text-xs font-light lg:text-base lg:font-normal">Order received</p>
+                    </div>
+                    <div>
+                        <h4 className="text-lg lg:text-3xl font-bold text-[#1F4B3F]">238M</h4>
+                        <p className="text-[#222222] text-xs font-light lg:text-base lg:font-normal">Projects Completed</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-1">
+                <img src={img} alt="" className=""/>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../../Components/Banner/Banner";
 
 const Home = () => {
     return (
-        <div className="h-screen">
-            <p>hello</p> 
+        <div className="">
+            <Banner/>
         </div>
     );
 };
