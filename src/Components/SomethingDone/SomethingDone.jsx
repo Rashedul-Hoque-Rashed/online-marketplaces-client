551a3885b78b28093e62f8486a78141e6a091cde
Rashed <rashedulhoqueceo@gmail.com>
2023-11-05 15:36:50 +0600

    implemant framer motion

--- src/Components/SomethingDone/SomethingDone.jsx
+++ src/Components/SomethingDone/SomethingDone.jsx
@@ -2,6 +2,7 @@ import icon1 from "../../assets/Icons/post-jobs.png"
 import icon2 from "../../assets/Icons/freelancer.png"
 import icon3 from "../../assets/Icons/pay.png"
 import icon4 from "../../assets/Icons/support.png"
+import { motion } from "framer-motion"
 
 const SomethingDone = () => {
     return (
@@ -12,22 +13,50 @@ const SomethingDone = () => {
             </div>
             <div className="grid grid-cols-2 md:grid-cols-4 justify-between gap-10">
                 <div className="flex-1">
-                    <img src={icon1} alt="" className="w-16 h-16 mb-2"/>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon1} alt="" className="w-16 h-16 mb-2" />
+                    </motion.div>
                     <h4 className="text-lg font-bold mb-2">Post a job</h4>
                     <p>It’s free and easy to post a job.</p>
                 </div>
                 <div className="flex-1">
-                    <img src={icon2} alt="" className="w-16 h-16 mb-2"/>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon2} alt="" className="w-16 h-16 mb-2" />
+                    </motion.div>
                     <h4 className="text-lg font-bold mb-2">Choose freelancers</h4>
                     <p>It’s free and easy to choose freelancers.</p>
                 </div>
                 <div className="flex-1">
-                    <img src={icon3} alt="" className="w-16 h-16 mb-2"/>
+                    <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon3} alt="" className="w-16 h-16 mb-2" />
+                    </motion.div>
                     <h4 className="text-lg font-bold mb-2">Pay safely</h4>
                     <p>It’s free and easy to pay safely.</p>
                 </div>
                 <div className="flex-1">
-                    <img src={icon4} alt="" className="w-16 h-16 mb-2"/>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon4} alt="" className="w-16 h-16 mb-2" />
+                    </motion.div>
                     <h4 className="text-lg font-bold mb-2">We’re here to help</h4>
                     <p>It’s free and easy to contact with us.</p>
                 </div>
--- src/Components/Trusted/Trusted.jsx
+++ src/Components/Trusted/Trusted.jsx
@@ -4,18 +4,61 @@ import icon3 from "../../assets/Icons/cisco.png";
 import icon4 from "../../assets/Icons/dropcam.png";
 import icon5 from "../../assets/Icons/logitech.png";
 import icon6 from "../../assets/Icons/spotify.png";
+import { motion } from "framer-motion"
 
 const Trusted = () => {
     return (
         <div className="bg-[#1F4B3F] py-10 rounded-t-2xl mx-4 ">
             <p className="text-sm font-medium text-white text-center mb-8">Trusted by the world’s best</p>
             <div className="container mx-auto px-20 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 items-center">
-                <img src={icon1} alt="" className="" />
-                <img src={icon2} alt="" className="" />
-                <img src={icon3} alt="" className="" />
-                <img src={icon4} alt="" className="" />
-                <img src={icon5} alt="" className="" />
-                <img src={icon6} alt="" className="" />
+            <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon1} alt="" className="" />
+                    </motion.div>
+                    <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon2} alt="" className="" />
+                    </motion.div>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon3} alt="" className="" />
+                    </motion.div>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon4} alt="" className="" />
+                    </motion.div>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon5} alt="" className="" />
+                    </motion.div>
+                <motion.div
+                     whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                        <img src={icon6} alt="" className="" />
+                    </motion.div>
             </div>
         </div>
     );
